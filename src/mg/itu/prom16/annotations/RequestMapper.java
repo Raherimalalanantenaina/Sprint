aea17095dcb1fc8c48fb896406aad8e1286591eb
Raherimalalanantenaina <168984775+Raherimalalanantenaina@users.noreply.github.com>
2024-12-09 15:52:15 +0100

    sprint13_2777
    validation

--- src/mg/itu/prom16/annotations/RequestMapper.java
+++ src/mg/itu/prom16/annotations/RequestMapper.java
@@ -3,7 +3,9 @@ package mg.itu.prom16.annotations;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 
+
 public class RequestMapper {
+
 
     public static <T> T mapRequestToObject(HttpServletRequest request, Class<T> clazz)throws Exception{
         try {
@@ -14,12 +16,13 @@ public class RequestMapper {
                 String paramName = (requestParam != null) ? requestParam.value() : field.getName();
                 String paramValue = request.getParameter(paramName);
     
-                if (paramValue != null) {
-                    field.setAccessible(true);
-                    field.set(instance, convertToFieldType(field.getType(), paramValue));
-                }
                 if (requestParam == null) {
                     throw new Exception("ETU2777   la param n'exite pas'" + paramName + "' is missing");
+                }
+                if (paramValue != null) {
+                    validation(field,paramValue);
+                    field.setAccessible(true);
+                    field.set(instance, convertToFieldType(field.getType(), paramValue));                   
                 }
     
             }
@@ -30,22 +33,68 @@ public class RequestMapper {
         }
     }
 
-    private static Object convertToFieldType(Class<?> fieldType, String value) {
-        if (fieldType == String.class) {
-            return value;
-        } else if (fieldType == int.class || fieldType == Integer.class) {
-            return Integer.parseInt(value);
-        } else if (fieldType == long.class || fieldType == Long.class) {
-            return Long.parseLong(value);
-        } else if (fieldType == boolean.class || fieldType == Boolean.class) {
-            return Boolean.parseBoolean(value);
-        } else if (fieldType == double.class || fieldType == Double.class) {
-            return Double.parseDouble(value);
-        } else if (fieldType == float.class || fieldType == Float.class) {
-            return Float.parseFloat(value);
-        } else {
-            throw new IllegalArgumentException("Unsupported field type: " + fieldType);
+
+
+
+
+
+    
+
+    private static Object convertToFieldType(Class<?> fieldType, String value) throws IllegalArgumentException {
+        try {
+            if (fieldType == String.class) {
+                return value;
+            } else if (fieldType == int.class || fieldType == Integer.class) {
+                return Integer.parseInt(value);
+            } else if (fieldType == long.class || fieldType == Long.class) {
+                return Long.parseLong(value);
+            } else if (fieldType == boolean.class || fieldType == Boolean.class) {
+                return Boolean.parseBoolean(value);
+            } else if (fieldType == double.class || fieldType == Double.class) {
+                return Double.parseDouble(value);
+            } else if (fieldType == float.class || fieldType == Float.class) {
+                return Float.parseFloat(value);
+            } else {
+                throw new IllegalArgumentException("Unsupported field type: " + fieldType);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Error converting value: " + value + " to type: " + fieldType, e);
         }
     }
+    
+
+
+
+    public static void validation(Field field, String paramValue) throws Exception {
+        if (field.isAnnotationPresent(Required.class) && (paramValue == null || paramValue.isEmpty())) {
+            throw new Exception("Le champ " + field.getName() + " ne doit pas être nul ou vide.");
+        }
+    
+        if (field.isAnnotationPresent(TypeDouble.class)) {
+            try {
+                Double.parseDouble(paramValue);
+            } catch (NumberFormatException e) {
+                throw new Exception("Le champ " + field.getName() + " doit être un nombre décimal valide.");
+            }
+        }
+    
+        if (field.isAnnotationPresent(TypeInt.class)) {
+            try {
+                Integer.parseInt(paramValue);
+            } catch (NumberFormatException e) {
+                throw new Exception("Le champ " + field.getName() + " doit être un entier valide.");
+            }
+        }
+    
+        if (field.isAnnotationPresent(TypeString.class)) {
+            // TypeString est toujours String, donc pas de conversion nécessaire
+            if (paramValue == null) {
+                throw new Exception("Le champ " + field.getName() + " doit être une chaîne non nulle.");
+            }
+        }
+    }
+
+
+    
 }
 
--- src/mg/itu/prom16/controllers/FrontController.java
+++ src/mg/itu/prom16/controllers/FrontController.java
@@ -6,9 +6,16 @@ import mg.itu.prom16.session.CustomSession;
 import mg.itu.prom16.map.Mapping;
 import mg.itu.prom16.map.VerbAction;
 import mg.itu.prom16.session.*;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.net.URLDecoder;
@@ -18,13 +25,15 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
-
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.Part;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@MultipartConfig
 public class FrontController extends HttpServlet {
     private List<String> controller = new ArrayList<>();
     private String controllerPackage;
@@ -46,108 +55,120 @@ public class FrontController extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-PrintWriter out = response.getWriter();
-String[] requestUrlSplitted = request.getRequestURL().toString().split("/");
-String controllerSearched = requestUrlSplitted[requestUrlSplitted.length - 1];
+        throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
+    String[] requestUrlSplitted = request.getRequestURL().toString().split("/");
+    String controllerSearched = requestUrlSplitted[requestUrlSplitted.length - 1];
 
-response.setContentType("text/html");
+    response.setContentType("text/html");
 
-try {
-    if (!error.isEmpty()) {
-        erreur = error;
-        detail = "Une erreur est survenue lors du traitement de la requête.";
-        throw new Exception(detail);
-    } else if (!lien.containsKey(controllerSearched)) {
-        erreur = "Erreur 404";
-        detail = "Methode non trouvee : " + controllerSearched;
-        throw new Exception(detail);
-    }
+    try {
+        // Vérification des erreurs
+        if (!error.isEmpty()) {
+            erreur = error;
+            detail = "Une erreur est survenue lors du traitement de la requête.";
+            throw new Exception(detail);
+        } else if (!lien.containsKey(controllerSearched)) {
+            erreur = "Erreur 2777";
+            detail = "Méthode non trouvée : " + controllerSearched;
+            throw new Exception(detail);
+        }
 
-    // Traitement de la requête
-    Mapping mapping = lien.get(controllerSearched);
-    Class<?> clazz = Class.forName(mapping.getClassName());
-    Object object = clazz.getDeclaredConstructor().newInstance();
-    Method method = null;
+        // Traitement de la requête
+        Mapping mapping = lien.get(controllerSearched);
+        Class<?> clazz = Class.forName(mapping.getClassName());
+        Object object = clazz.getDeclaredConstructor().newInstance();
+        Method method = null;
 
-    // Vérification du verbe HTTP
-    if (!mapping.isVerbPresent(request.getMethod())) {
-        erreur = "Verbe HTTP non supporte";
-        detail = "Le verbe " + request.getMethod() + " n'est pas supporte pour cette action.";
-        throw new Exception(detail);
-    }
+        // Vérification du verbe HTTP
+        if (!mapping.isVerbPresent(request.getMethod())) {
+            erreur = "Verbe HTTP non supporté";
+            detail = "Le verbe " + request.getMethod() + " n'est pas supporté pour cette action.";
+            throw new Exception(detail);
+        }
 
-    // Recherche de la méthode correspondante
-    for (Method m : clazz.getDeclaredMethods()) {
-        for (VerbAction action : mapping.getVerb()) {
-            if (m.getName().equals(action.getAction()) && action.getVerb().equalsIgnoreCase(request.getMethod())) {
-                method = m;
+        // Recherche de la méthode correspondante
+        for (Method m : clazz.getDeclaredMethods()) {
+            for (VerbAction action : mapping.getVerb()) {
+                if (m.getName().equals(action.getAction()) && action.getVerb().equalsIgnoreCase(request.getMethod())) {
+                    method = m;
+                    break;
+                }
+            }
+            if (method != null) {
                 break;
             }
         }
-        if (method != null) {
-            break;
+
+        // Vérification de la méthode trouvée
+        if (method == null) {
+            error = "Erreur 404";
+            detail = "Aucune méthode correspondante trouvée pour : " + controllerSearched;
+            throw new Exception(detail);
         }
-    }
 
-    // Vérification de la méthode trouvée
-    if (method == null) {
-        error = "Erreur 404";
-        detail = "Aucune methode correspondante trouvee pour : " + controllerSearched;
-        throw new Exception(detail);
-    }
+        // Injecter les paramètres dans la méthode
+        Object[] parameters = getMethodParameters(method, request);
+        Object returnValue = method.invoke(object, parameters);
 
-    // Injecter les paramètres dans la méthode
-    Object[] parameters = getMethodParameters(method, request);
-    Object returnValue = method.invoke(object, parameters);
-
-    // Gestion de l'API REST
-    if (method.isAnnotationPresent(RestApi.class)) {
-        response.setContentType("application/json");
-        Gson gson = new Gson();
-        if (returnValue instanceof String) {
-            String jsonResponse = gson.toJson(returnValue);
-            out.println(jsonResponse);
+        // Gestion de l'API REST
+        if (method.isAnnotationPresent(RestApi.class)) {
+            response.setContentType("application/json");
+            Gson gson = new Gson();
+            if (returnValue instanceof String) {
+                String jsonResponse = gson.toJson(returnValue);
+                out.println(jsonResponse);
+            } else if (returnValue instanceof ModelView) {
+                ModelView modelView = (ModelView) returnValue;
+                String jsonResponse = gson.toJson(modelView.getData());
+                out.println(jsonResponse);
+            } else {
+                out.println("Type de données non reconnu");
+            }
+        } else if (returnValue instanceof String) {
+            out.println("Méthode trouvée dans " + returnValue);
         } else if (returnValue instanceof ModelView) {
             ModelView modelView = (ModelView) returnValue;
-            String jsonResponse = gson.toJson(modelView.getData());
-            out.println(jsonResponse);
+            for (Map.Entry<String, Object> entry : modelView.getData().entrySet()) {
+                request.setAttribute(entry.getKey(), entry.getValue());
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher(modelView.getUrl());
+            dispatcher.forward(request, response);
         } else {
-            out.println("Type de donnees non reconnu");
+            out.println("Type de données non reconnu");
         }
-    } else if (returnValue instanceof String) {
-        out.println("Methode trouvee dans " + returnValue);
-    } else if (returnValue instanceof ModelView) {
-        ModelView modelView = (ModelView) returnValue;
-        for (Map.Entry<String, Object> entry : modelView.getData().entrySet()) {
-            request.setAttribute(entry.getKey(), entry.getValue());
-        }
-        RequestDispatcher dispatcher = request.getRequestDispatcher(modelView.getUrl());
-        dispatcher.forward(request, response);
-    } else {
-        out.println("Type de données non reconnu");
+
+    } catch (Exception e) {
+        // // Gestion des erreurs
+        erreur = "Une Erreur Nantenaina";
+        detail = e.getMessage();
+ 
+        // Générez dynamiquement la page d'erreur
+// Générez dynamiquement la page d'erreur avec du CSS
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Erreur</title>");
+            // Ajout de CSS intégré pour styliser la page
+            out.println("<style>");
+            out.println("body { font-family: Arial, sans-serif; background-color: #f8f9fa; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh; }");
+            out.println(".error-container { background-color: #ffffff; border: 1px solid #e0e0e0; padding: 30px; border-radius: 8px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); max-width: 600px; text-align: center; }");
+            out.println("h1 { color: #dc3545; font-size: 2em; margin-bottom: 20px; }");
+            out.println("p { color: #6c757d; font-size: 1.1em; }");
+            out.println("</style>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<div class='error-container'>");
+            out.println("<h1>" + erreur + "</h1>");
+            out.println("<p>" + detail + "</p>");
+            out.println("</div>");
+            out.println("</body>");
+            out.println("</html>");
+
+
+
+    } finally {
+        out.close(); // Toujours fermer le PrintWriter
     }
-
-} catch (Exception e) {
-    // // Gestion des erreurs
-    erreur = (erreur.isEmpty()) ? "Erreur" : erreur; 
-    detail = (detail.isEmpty()) ? e.getMessage() : detail;
-
-    // Générez dynamiquement la page d'erreur
-    out.println("<html>");
-    out.println("<head><title>Erreur</title></head>");
-    out.println("<body>");
-    out.println("<h1>" + erreur + "</h1>");
-    out.println("<p>" + detail + "</p>");
-    out.println("</body>");
-    out.println("</html>");
-
-
-    // RequestDispatcher dispatcher = request.getRequestDispatcher("/mg/itu/prom16/erreurweb/erreur.jsp");
-    // dispatcher.forward(request, response);
-} finally {
-    out.close(); // Toujours fermer le PrintWriter
-}
 }
 
 
@@ -235,15 +256,48 @@ try {
             try {
                 if (parameters[i].isAnnotationPresent(Param.class)) {
                     Param param = parameters[i].getAnnotation(Param.class);
-                    String paramValue = request.getParameter(param.value());
-
-                    // Vérifiez si le paramètre est nul ou vide
+                     
                     if (param == null) {
                         throw new Exception("ETU2777 Le paramètre " + param.value() + " est manquant.");
                     }
 
-                    parameterValues[i] = paramValue; // En supposant que tous les paramètres sont des chaînes pour simplifier
+                    if(parameters[i].getType().equals(Part.class)){
+                        try {
+                            Part filePart = request.getPart(param.value());
+                            String fileName = filePart.getSubmittedFileName();
+                        
+                            // Définir le répertoire où enregistrer le fichier
+                            String uploadDirectory = "C:/uploads/";
+                            File uploadDir = new File(uploadDirectory);
+                        
+                            // Vérifier si le répertoire existe, sinon le créer
+                            if (!uploadDir.exists()) {
+                                uploadDir.mkdirs(); // Crée le répertoire et ses parents si nécessaire
+                            }
+                        
+                            File file = new File(uploadDirectory, fileName);
+                        
+                            // Enregistrer le fichier
+                            try (InputStream input = filePart.getInputStream();
+                                 FileOutputStream output = new FileOutputStream(file)) {
+                                byte[] buffer = new byte[1024];
+                                int bytesRead;
+                                while ((bytesRead = input.read(buffer)) != -1) {
+                                    output.write(buffer, 0, bytesRead);
+                                }
+                            }
+                            parameterValues[i] = filePart;
+                        } catch (Exception e) {
+                            e.printStackTrace(); // Affiche l'erreur dans la console pour le débogage
+                        }                        
+                        // String fileName = getFileName(filePart);
+                    }
+                    else{
+                        String paramValue = request.getParameter(param.value());
+                        parameterValues[i] = paramValue; // En supposant que tous les paramètres sont des chaînes pour simplifier
+                    }
                 }
+                
                 if (parameters[i].isAnnotationPresent(RequestObject.class)) {
                     parameterValues[i] = RequestMapper.mapRequestToObject(request, parameters[i].getType());
                 }
